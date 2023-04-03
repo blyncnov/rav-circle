8e3781ec2eef2cee058bb042d3ddd64980e72328
blyncnov <bholuwatife00@gmail.com>
2023-04-03 08:11:04 +0100

    rav => Fixed dashboard layout issue

--- client/layout/dashboard.tsx
+++ client/layout/dashboard.tsx
@@ -1,26 +1,28 @@
-import React from "react";
+export const dashboard = "dashboard not initiated";
 
-// Dashboard layout Components
-import Sidebar from "@/components/Dashboard/Sidebar";
-import Topbar from "@/components/Dashboard/Topbar";
+// import React from "react";
 
-const DashboardLayout = ({ children }: any) => {
-  return (
-    <>
-      <div className="dashboard_grid_layout">
-        <div className="dashboard_grid_column">
-          <Sidebar />
-        </div>
+// // Dashboard layout Components
+// import Sidebar from "@/components/Dashboard/Sidebar";
+// import Topbar from "@/components/Dashboard/Topbar";
 
-        <div className="dashboard_main">
-          <div className="dashboard_grid_column">
-            <Topbar />
-            <div className="main_content">{children}</div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
+// const DashboardLayout = ({ children }: any) => {
+//   return (
+//     <>
+//       <div className="dashboard_grid_layout">
+//         <div className="dashboard_grid_column">
+//           <Sidebar />
+//         </div>
 
-export default DashboardLayout;
+//         <div className="dashboard_main">
+//           <div className="dashboard_grid_column">
+//             <Topbar />
+//             <div className="main_content">{children}</div>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default DashboardLayout;
